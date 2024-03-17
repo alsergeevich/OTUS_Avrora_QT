58f440b7b0b2112c5a60a5ae58dbec96dc72e9c8
Aleksandr <alsergeevich@gmail.com>
2024-03-17 17:27:18 +0500

    ДЗ №8 работа с картой

---
+++ PositioningAndLocation_template/translations/ru.auroraos.PositioningAndLocation-ru.ts
@@ -0,0 +1,50 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru">
+<context>
+    <name>DefaultCoverPage</name>
+    <message>
+        <location filename="../qml/cover/DefaultCoverPage.qml" line="46"/>
+        <source>PositioningAndLocation</source>
+        <translation>Геопозиция и карты</translation>
+    </message>
+</context>
+<context>
+    <name>PositioningPage</name>
+    <message>
+        <location filename="../qml/pages/PositioningPage.qml" line="32"/>
+        <source>Timestamp</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PositioningPage.qml" line="38"/>
+        <source>Coordinate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PositioningPage.qml" line="44"/>
+        <source>Latitude</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PositioningPage.qml" line="50"/>
+        <source>Longitude</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PositioningPage.qml" line="56"/>
+        <source>Altitude</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PositioningPage.qml" line="62"/>
+        <source>Speed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PositioningPage.qml" line="63"/>
+        <source>%1 meters / second</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
